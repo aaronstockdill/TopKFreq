ecd6c0915cc318c59e592fd8ee01c57e54d4c7b8
Aaron Stockdill <aaronstockdill@me.com>
2020-03-07 10:14:24 +0000

    Add multipush and multipop macros

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 ASM=nasm
-ASMFLAGS=-f macho64 -g
+ASMFLAGS=-f macho64 -w-macro-defaults -w-macro-params -g
 
 LD=ld
 LDFLAGS=-macosx_version_min 10.13.0 -lSystem
--- topk.asm
+++ topk.asm
@@ -23,6 +23,20 @@
     syscall
 %endmacro
 
+%macro multipush 1-*
+  %rep %0
+    push %1
+  %rotate 1
+  %endrep
+%endmacro
+
+%macro multipop 1-*
+  %rep %0
+  %rotate -1
+    pop %1
+  %endrep
+%endmacro
+
 
 
     global _main
